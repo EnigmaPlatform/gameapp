179c07f9b90b8a9828beea9d3ea37cfac87846f4
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-05-16 11:10:59 +1100

    Update leveling.py

--- modules/leveling.py
+++ modules/leveling.py
@@ -1,1 +1,23 @@
-
+class LevelSystem:
+    def __init__(self):
+        self.level = 1
+        self.experience = 0
+        self.experience_to_next_level = 100
+        
+    def add_experience(self, amount):
+        self.experience += amount
+        if self.experience >= self.experience_to_next_level:
+            self.level_up()
+            
+    def level_up(self):
+        self.level += 1
+        self.experience -= self.experience_to_next_level
+        self.experience_to_next_level = int(self.experience_to_next_level * 1.2)
+        return True  # Для сигнала о новом уровне
+    
+    def get_level_info(self):
+        return {
+            'level': self.level,
+            'experience': self.experience,
+            'next_level': self.experience_to_next_level
+        }
