45ed867c4e8f1817ad26389671b40286a3b3ed88
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-05-16 11:10:49 +1100

    Update character.py

--- modules/character.py
+++ modules/character.py
@@ -1,1 +1,23 @@
-
+class Character:
+    def __init__(self):
+        self.stats = {
+            'strength': 1,
+            'agility': 1,
+            'intelligence': 1,
+            'charisma': 1,
+            'endurance': 1
+        }
+        self.talent_points = 10  # Начальные очки
+        
+    def increase_stat(self, stat_name):
+        if self.talent_points > 0 and stat_name in self.stats:
+            self.stats[stat_name] += 1
+            self.talent_points -= 1
+            return True
+        return False
+    
+    def get_stats(self):
+        return self.stats.copy()
+    
+    def get_talent_points(self):
+        return self.talent_points
